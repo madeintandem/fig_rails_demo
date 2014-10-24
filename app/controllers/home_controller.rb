b95bb21c1bcb653a83ee039892fc08bd4b684872
Joe Hirn <joseph.hirn@gmail.com>
2014-10-24 14:23:43 -0500

    display postgres connection information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @es_client= Elasticsearch::Model.client
+    @es_client = Elasticsearch::Model.client
+    @pg_config = ActiveRecord::Base.connection_config
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def print_hash(hash)
+    raw JSON.pretty_generate(hash).gsub(" ","&nbsp;")
+  end
 end
